09db957ac72c9c1460fcd4d1cff6df4fc08047f8
Djiby Diop <djibril diop>
2026-09-17 15:47:37 +0000

    Ajout des champs prenom et telephone au formulaire contact

--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -8,16 +8,23 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './contact.html',
 })
 export class Contact {
+  prenom: string = '';
   nom: string = '';
+  telephone: string = '';
   email: string = '';
   message: string = '';
   envoye: boolean = false;
 
   envoyerMessage() {
-    if (this.nom && this.email && this.message) {
+    if (this.prenom && this.nom && this.email && this.message) {
       this.envoye = true;
-      console.log('Message envoyé:',
-         { nom: this.nom, email: this.email, message: this.message });
+      console.log('Message envoyé:', {
+        prenom: this.prenom,
+        nom: this.nom,
+        telephone: this.telephone,
+        email: this.email,
+        message: this.message
+      });
     }
   }
 }
