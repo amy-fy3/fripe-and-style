3c3053b780261d5cb9a7ad5a350afe47c2802289
Amy Faye <amy_faye11@outlook.com>
2026-09-17 12:04:51 +0100

    Correction detail-produit avec affichage complet des donnees

--- src/app/pages/detail-produit/detail-produit.ts
+++ src/app/pages/detail-produit/detail-produit.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { VETEMENTS, Vetement } from '../../vetements';
 
 @Component({
-  imports: [],
+  imports: [CommonModule, RouterLink],
   selector: 'app-detail-produit',
   styleUrl: './detail-produit.css',
   templateUrl: './detail-produit.html',
 })
-export class DetailProduit {}
+export class DetailProduit {
+  vetement: Vetement | undefined;
+
+  constructor(private route: ActivatedRoute) {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.vetement = VETEMENTS.find(v => v.id === id);
+  }
+}
