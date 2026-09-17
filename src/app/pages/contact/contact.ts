1d82d6ff7da24e861fae59a681344903c4f89b8f
Djiby Diop <djibril diop>
2026-09-17 14:58:58 +0000

    Ajout des pages A propos et Contact avec formulaire

--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  imports: [],
+  imports: [FormsModule],
   selector: 'app-contact',
   styleUrl: './contact.css',
   templateUrl: './contact.html',
 })
-export class Contact {}
+export class Contact {
+  nom: string = '';
+  email: string = '';
+  message: string = '';
+  envoye: boolean = false;
+
+  envoyerMessage() {
+    if (this.nom && this.email && this.message) {
+      this.envoye = true;
+      console.log('Message envoyé:',
+         { nom: this.nom, email: this.email, message: this.message });
+    }
+  }
+}
