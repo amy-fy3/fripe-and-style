f7ed6e251ed545778ac09af6440442bfbb9a689c
Amy Faye <amy_faye11@outlook.com>
2026-09-17 13:07:08 +0100

    Ajout du design CSS pour navbar, footer, catalogue et page detail

--- src/app/navbar/navbar.spec.ts
+++ src/app/navbar/navbar.spec.ts
@@ -1,21 +1,10 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { Navbar } from './navbar';
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
-describe('Navbar', () => {
-  let component: Navbar;
-  let fixture: ComponentFixture<Navbar>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [Navbar],
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(Navbar);
-    component = fixture.componentInstance;
-    await fixture.whenStable();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+@Component({
+  imports: [RouterLink],
+  selector: 'app-navbar',
+  styleUrl: './navbar.css',
+  templateUrl: './navbar.html',
+})
+export class Navbar {}
